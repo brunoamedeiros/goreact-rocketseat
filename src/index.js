ef4d5b9024979abf31e142df4ed1e0d1427a3c8a
SISTEMAFIESC\bruno.medeiros <bruno.medeiros@sc.senai.br>
2018-10-03 18:17:06 -0300

    Configurando Prettier e finalizando Módulo 0.

--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
-import React, { Component, Fragment } from 'react';
-import { render } from 'react-dom';
+import React, { Component, Fragment } from "react";
+import { render } from "react-dom";
 
-import Button from './Button';
+import Button from "./Button";
 
-import './style.scss';
+import "./style.scss";
 
 class App extends Component {
   state = {
-    counter: 0,
+    counter: 0
   };
 
   // Montagem / Atualização
@@ -44,12 +44,12 @@ class App extends Component {
   // }
 
   handleClick = () => {
-    this.setState({
-      counter: this.state.counter + 1,
-    });
+    // this.setState({
+    //   counter: this.state.counter + 1
+    // });
 
     // Alterar variavel o estado na fila
-    // this.setState(state => ({ counter: this.state.counter + 1}));
+    this.setState(state => ({ counter: state.counter + 1 }));
 
     // Com callback
     // this.setState({ counter: this.state.counter + 1}, () => {
@@ -61,12 +61,12 @@ class App extends Component {
     return (
       <Fragment>
         <h1 className="title">Oi</h1>
-        <h2 style={{ color: '#F00' }}>{this.state.counter}</h2>
+        <h2 style={{ color: "#F00" }}>{this.state.counter}</h2>
         <Button onClick={this.handleClick}>Somar</Button>
       </Fragment>
     );
-  };
+  }
 }
 
-render(<App />, document.getElementById('app'));
+render(<App />, document.getElementById("app"));
 // render(<App initialCounter={0} />, document.getElementById('app'));
